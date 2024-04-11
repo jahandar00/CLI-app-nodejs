1696f467dcad2eff0186c6e22141ddcce731e49c
jahandar <axiyev.cahandar@gmail.com>
2024-04-11 05:17:20 +0200

    category added

--- index.js
+++ index.js
@@ -6,22 +6,83 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-const jokeGenerator = () => {
-    const req = https.request("https://api.chucknorris.io/jokes/random",
-    function (res) {
-        res.on("data", function (data) {
-            console.log(JSON.parse(data.toString()));
-        })
-    }
-)
+// const jokeGenerator = () => {
+//     const req = https.request("https://api.chucknorris.io/jokes/random",
+//     function (res) {
+//         let data = '';
+//         res.on("data", function (chunk) {
+//             data += chunk;
+//         });
+//         res.on("end", function () {
+//             console.log(JSON.parse(data).value);
+//         });
+//     });
+
+//     req.on("error", function (error) {
+//         console.error("Error making request:", error);
+//     });
+
+//     req.end();
+// }
+
+function generateRandomNumber(min, max) {
+    // Generate a random number between min (inclusive) and max (exclusive)
+    return Math.floor(Math.random() * (max - min) + min);
 }
+
+
+const jokeCategories = () => {
+    const req = https.request("https://api.chucknorris.io/jokes/categories",
+    function (res) {
+        let data = '';
+        res.on("data", function (chunk) {
+            data += chunk;
+        });
+        res.on("end", function () {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    req.on("error", function (error) {
+        console.error("Error making request:", error);
+    });
+
+    req.end();
+}
+
+const jokeCategoryRequest = (category) => {
+    const req = https.request(`https://api.chucknorris.io/jokes/search?query=${category}`,
+    function (res) {
+        let data = '';
+        res.on("data", function (chunk) {
+            data += chunk;
+        });
+        res.on("end", function () {
+            const parsedData = JSON.parse(data)
+            const randomNumber = generateRandomNumber(1, parsedData.result.length);
+            console.log(parsedData.result[randomNumber].value);
+            
+        });
+    });
+
+    req.on("error", function (error) {
+        console.error("Error making request:", error);
+    });
+
+    req.end();
+}
+
 
 rl.question("What is your name? ", function(name){
     rl.question(
         "Hi, " + name + ", do you want to hear some jokes? [Y/N] ",
         function (answer) {
             if(answer === "Y"){
-                console.log("");
+                rl.question("Select one of the categories below: ", function(caterogy){
+                    jokeCategoryRequest(caterogy);
+                });
+                jokeCategories();
+                
             } else {
                 console.log("Get the hell out of here!");
                 rl.close();
