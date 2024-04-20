0ad586a35bea42c3733ecb755215b35a17496782
jahandar <axiyev.cahandar@gmail.com>
2024-04-20 17:12:27 +0200

    unimportant functions deleted

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ function generateRandomNumber(min, max) {
 
 const jokeCategories = () => {
     const req = https.request("https://api.chucknorris.io/jokes/categories",
-    function (res) {
-        let data = '';
-        res.on("data", function (chunk) {
-            data += chunk;
+        function (res) {
+            let data = '';
+            res.on("data", function (chunk) {
+                data += chunk;
+            });
+            res.on("end", function () {
+                for (let i = 0; i < JSON.parse(data).length; i++) {
+                    console.log(`${i + 1}. ${JSON.parse(data)[i]}`);
+                }
+                ;
+            });
         });
-        res.on("end", function () {
-            for(let i = 0; i < JSON.parse(data).length; i++){
-                console.log(`${i + 1}. ${JSON.parse(data)[i]}`);
-            }
-            ;
-        });
-    });
 
     req.on("error", function (error) {
         console.error("Error making request:", error);
@@ -37,18 +37,18 @@ const jokeCategories = () => {
 
 const jokeCategoryRequest = (category) => {
     const req = https.request(`https://api.chucknorris.io/jokes/search?query=${category}`,
-    function (res) {
-        let data = '';
-        res.on("data", function (chunk) {
-            data += chunk;
+        function (res) {
+            let data = '';
+            res.on("data", function (chunk) {
+                data += chunk;
+            });
+            res.on("end", function () {
+                const parsedData = JSON.parse(data)
+                const randomNumber = generateRandomNumber(1, parsedData.result.length);
+                console.log(parsedData.result[randomNumber].value);
+
+            });
         });
-        res.on("end", function () {
-            const parsedData = JSON.parse(data)
-            const randomNumber = generateRandomNumber(1, parsedData.result.length);
-            console.log(parsedData.result[randomNumber].value);
-            
-        });
-    });
 
     req.on("error", function (error) {
         console.error("Error making request:", error);
@@ -58,16 +58,17 @@ const jokeCategoryRequest = (category) => {
 }
 
 
-rl.question("What is your name? ", function(name){
+rl.question("What is your name? ", function (name) {
     rl.question(
         "Hi, " + name + ", do you want to hear some jokes? [Y/N] ",
         function (answer) {
-            if(answer === "Y"){
-                jokeCategories()
-                rl.question("Select one of the categories below:\n ", function(caterogy){
+            if (answer === "Y") {
+                jokeCategories();
+                rl.question("Select one of the categories below:\n ", function (caterogy) {
                     jokeCategoryRequest(caterogy);
+                    rl.close();
                 })
-                
+
             } else {
                 console.log("Get the hell out of here!");
                 rl.close();
